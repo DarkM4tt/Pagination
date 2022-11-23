3a90a25cf3de5d1565a1da1aa71c148a5f7dccd7
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-11-23 21:42:18 +0530

    Number pages completed.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const apiLink = "https://jsonplaceholder.typicode.com/todos";
+
 function App() {
   const [todos, setTodos] = useState([]);
-  const apiLink = "https://jsonplaceholder.typicode.com/todos";
+  const [todosPerPage, setTodosPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const numOfTotalPages = Math.ceil(todos.length / todosPerPage);
+  const pages = [...Array(numOfTotalPages + 1).keys()].slice(1);
+
+  const indexOfLastTodo = currentPage * todosPerPage;
+  const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+
+  const visibleTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
 
   async function fetchAPI() {
     await fetch(apiLink)
@@ -16,11 +27,26 @@ function App() {
     fetchAPI();
   }, []);
 
+  const prevPageHandler = () => {};
+
+  const nextPageHandler = () => {};
+
   return (
     <div>
-      {todos.map((todo) => (
+      {visibleTodos.map((todo) => (
         <p key={todo.id}>{todo.title}</p>
       ))}
+      <span onClick={prevPageHandler}>Prev</span>
+      <p>
+        {pages.map((page) => (
+          <span
+            key={page}
+            onClick={() => setCurrentPage(page)}
+            className={`${currentPage === page ? "active" : ""}`}
+          >{`${page} | `}</span>
+        ))}
+      </p>
+      <span onClick={nextPageHandler}>Next</span>
     </div>
   );
 }
